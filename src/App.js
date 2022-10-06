c2058041a37f30ec5b94666fe5b88a1c1d9dfb36
09eric09 <09eric09@gmail.com>
2022-10-06 10:30:05 -0700

    installed materialize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 function App() {
+  useEffect(()=> {
+    //Initialize JS
+    M.AutoInit();
+  }, []);
+  
   return (
     <>
     
