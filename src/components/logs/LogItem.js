02d0af8263a9a2dfbcabb0584e095c85e7789296
09eric09 <09eric09@gmail.com>
2022-10-07 11:17:15 -0700

    finished LogItem

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const LogItem = ({log}) => {
   return (
@@ -6,6 +7,13 @@ const LogItem = ({log}) => {
     <li className={'collection-item'}>
     <div>
       <a className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`} href="#edit-log-modal">{log.message}</a>
+      <br />
+      <span className="grey-text">
+      <span className='black-text bold'>ID #{log.id}</span> last updated by 
+      <span className='black-text bold'> {log.tech}</span> on {''}
+      <span className='black-text'><Moment format={'MM/DD/YY'}>{log.date}</Moment></span>
+      </span>
+      <a href="#" className="secondary-content"><i className="material-icons grey-text">delete</i></a>
     </div>
     </li>
     </>
