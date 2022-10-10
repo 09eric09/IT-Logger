dfbc90a981b7f8426bd5d5a06aa818873413f7bb
09eric09 <09eric09@gmail.com>
2022-10-10 12:46:34 -0700

    added search funcitonality for logs

--- src/components/UI/SearchBar.js
+++ src/components/UI/SearchBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { logActions } from '../../store';
 
 const SearchBar = () => {
-  const [log, setLog] = useState('');
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
+
+  useEffect(()=> {
+    fetch(`/logs/?q=${search}`)
+    .then(res => res.json())
+    .then(data => dispatch(logActions.replaceLogs(data)));
+  }, [search, dispatch]);
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        dispatch(logActions.addLog(log));
     }
 
   return (
@@ -17,7 +22,7 @@ const SearchBar = () => {
     <div className="nav-wrapper">
       <form onSubmit={onSubmitHandler}>
         <div className="input-field">
-          <input onChange={(e)=> setLog(e.target.value)} id="search" type="search"/>
+          <input onChange={(e)=> setSearch(e.target.value)} id="search" type="search" placeholder='Search Logs...'/>
           <label className="label-icon"><i className="material-icons">search</i></label>
           <i className="material-icons">close</i>
         </div>
